df69ef11bb802e19956e0a75fac91afed3debc57
garciamilord <garciamilord@gmail.com>
2021-09-27 15:04:27 -0400

    window

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace MineSweeper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.easy = new System.Windows.Forms.RadioButton();
+            this.medium = new System.Windows.Forms.RadioButton();
+            this.expert = new System.Windows.Forms.RadioButton();
+            this.customs = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 130);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Play);
+            // 
+            // easy
+            // 
+            this.easy.AutoSize = true;
+            this.easy.Location = new System.Drawing.Point(171, 66);
+            this.easy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.easy.Name = "easy";
+            this.easy.Size = new System.Drawing.Size(60, 21);
+            this.easy.TabIndex = 1;
+            this.easy.TabStop = true;
+            this.easy.Text = "Easy";
+            this.easy.UseVisualStyleBackColor = true;
+            this.easy.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // medium
+            // 
+            this.medium.AutoSize = true;
+            this.medium.Checked = true;
+            this.medium.Location = new System.Drawing.Point(265, 66);
+            this.medium.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.medium.Name = "medium";
+            this.medium.Size = new System.Drawing.Size(78, 21);
+            this.medium.TabIndex = 2;
+            this.medium.TabStop = true;
+            this.medium.Text = "Medium";
+            this.medium.UseVisualStyleBackColor = true;
+            this.medium.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // expert
+            // 
+            this.expert.AutoSize = true;
+            this.expert.Location = new System.Drawing.Point(416, 66);
+            this.expert.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.expert.Name = "expert";
+            this.expert.Size = new System.Drawing.Size(69, 21);
+            this.expert.TabIndex = 3;
+            this.expert.TabStop = true;
+            this.expert.Text = "Expert";
+            this.expert.UseVisualStyleBackColor = true;
+            this.expert.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // customs
+            // 
+            this.customs.AutoSize = true;
+            this.customs.Location = new System.Drawing.Point(531, 66);
+            this.customs.Margin = new System.Windows.Forms.Padding(4);
+            this.customs.Name = "customs";
+            this.customs.Size = new System.Drawing.Size(76, 21);
+            this.customs.TabIndex = 4;
+            this.customs.TabStop = true;
+            this.customs.Text = "Custom";
+            this.customs.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 220);
+            this.Controls.Add(this.customs);
+            this.Controls.Add(this.expert);
+            this.Controls.Add(this.medium);
+            this.Controls.Add(this.easy);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.Text = "MineSweeper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton easy;
+        private System.Windows.Forms.RadioButton medium;
+        private System.Windows.Forms.RadioButton expert;
+        private System.Windows.Forms.RadioButton custom;
+        private System.Windows.Forms.RadioButton customs;
+    }
+}
+
